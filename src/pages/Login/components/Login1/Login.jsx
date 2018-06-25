48187d78d14a3907edef4437dcf15a68baad5071
leo <leozhang621@gmail.com>
2018-06-25 11:56:26 +0800

    add alias,rebuild login page

--- src/pages/Login/components/Login1/Login.jsx
+++ src/pages/Login/components/Login1/Login.jsx
@@ -8,138 +8,7 @@ import {
 } from '@icedesign/form-binder';
 import IceIcon from '@icedesign/icon';
 import './Login.scss';
-
-const { Row, Col } = Grid;
-
-export default class Login extends Component {
-  static displayName = 'Login';
-
-  static propTypes = {};
-
-  static defaultProps = {};
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: {
-        account: '',
-        password: '',
-        checkbox: false,
-      },
-    };
-  }
-
-  formChange = (value) => {
-    this.setState({
-      value,
-    });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.refs.form.validateAll((errors, values) => {
-      if (errors) {
-        console.log('errors', errors);
-        return;
-      }
-      console.log('values:', values);
-      Feedback.toast.success('登录成功');
-      // 登录成功后做对应的逻辑处理
-    });
-  };
-
-  render() {
-    return (
-      <div style={styles.container} className="user-login">
-        <div style={styles.header}>
-          <a href="#" style={styles.meta}>
-            <img
-              style={styles.logo}
-              src="https://img.alicdn.com/tfs/TB13UQpnYGYBuNjy0FoXXciBFXa-242-134.png"
-              alt="logo"
-            />
-            <span style={styles.title}>飞冰</span>
-          </a>
-          <p style={styles.desc}>飞冰让前端开发简单而友好</p>
-        </div>
-        <div style={styles.formContainer}>
-          <h4 style={styles.formTitle}>登 录</h4>
-          <IceFormBinderWrapper
-            value={this.state.value}
-            onChange={this.formChange}
-            ref="form"
-          >
-            <div style={styles.formItems}>
-              <Row style={styles.formItem}>
-                <Col style={styles.formItemCol}>
-                  <IceIcon
-                    type="person"
-                    size="small"
-                    style={styles.inputIcon}
-                  />
-                  <IceFormBinder name="account" required message="必填">
-                    <Input
-                      size="large"
-                      maxLength={20}
-                      placeholder="管理员账号"
-                    />
-                  </IceFormBinder>
-                </Col>
-                <Col>
-                  <IceFormError name="account" />
-                </Col>
-              </Row>
-
-              <Row style={styles.formItem}>
-                <Col style={styles.formItemCol}>
-                  <IceIcon type="lock" size="small" style={styles.inputIcon} />
-                  <IceFormBinder name="password" required message="必填">
-                    <Input
-                      size="large"
-                      htmlType="password"
-                      placeholder="密码"
-                    />
-                  </IceFormBinder>
-                </Col>
-                <Col>
-                  <IceFormError name="password" />
-                </Col>
-              </Row>
-
-              <Row style={styles.formItem}>
-                <Col>
-                  <IceFormBinder name="checkbox">
-                    <Checkbox style={styles.checkbox}>记住账号</Checkbox>
-                  </IceFormBinder>
-                </Col>
-              </Row>
-
-              <Row style={styles.formItem}>
-                <Button
-                  type="primary"
-                  onClick={this.handleSubmit}
-                  style={styles.submitBtn}
-                >
-                  登 录
-                </Button>
-              </Row>
-
-              <Row className="tips" style={styles.tips}>
-                <a href="/" style={styles.link}>
-                  立即注册
-                </a>
-                <span style={styles.line}>|</span>
-                <a href="/" style={styles.link}>
-                  忘记密码
-                </a>
-              </Row>
-            </div>
-          </IceFormBinderWrapper>
-        </div>
-      </div>
-    );
-  }
-}
+import { login } from '@/service'
 
 const styles = {
   container: {
@@ -232,3 +101,127 @@ const styles = {
     margin: '0 8px',
   },
 };
+const { Row, Col } = Grid;
+const Toast = Feedback.toast;
+
+export default class Login extends Component {
+  static displayName = 'Login';
+
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: {
+        account: '',
+        password: '',
+      },
+    };
+  }
+
+  formChange = (value) => {
+    this.setState({
+      value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.refs.form.validateAll(async (errors, values) => {
+      if (errors) {
+        console.log('errors', errors);
+      } else {
+        const res = await this.login(values).catch(()=>false)
+        if(res) {
+          Toast.success('登录成功')
+        } else {
+          Toast.error('登录错误')
+        }
+      }
+    })
+  }
+
+  login = (data) => {
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        reject(data)
+      },3000)
+    })
+  }
+
+  render() {
+    return (
+      <div style={styles.container} className="user-login">
+        <div style={styles.header}>
+          <a href="#" style={styles.meta}>
+            <img
+              style={styles.logo}
+              src="https://img.alicdn.com/tfs/TB13UQpnYGYBuNjy0FoXXciBFXa-242-134.png"
+              alt="logo"
+            />
+            <span style={styles.title}>飞冰</span>
+          </a>
+          <p style={styles.desc}>飞冰让前端开发简单而友好</p>
+        </div>
+        <div style={styles.formContainer}>
+          <h4 style={styles.formTitle}>登 录</h4>
+          <IceFormBinderWrapper
+            value={this.state.value}
+            onChange={this.formChange}
+            ref="form"
+          >
+            <div style={styles.formItems}>
+              <Row style={styles.formItem}>
+                <Col style={styles.formItemCol}>
+                  <IceIcon
+                    type="person"
+                    size="small"
+                    style={styles.inputIcon}
+                  />
+                  <IceFormBinder name="account" required message="请输入账号">
+                    <Input
+                      size="large"
+                      maxLength={20}
+                      placeholder="账号"
+                    />
+                  </IceFormBinder>
+                </Col>
+                <Col>
+                  <IceFormError name="account" />
+                </Col>
+              </Row>
+
+              <Row style={styles.formItem}>
+                <Col style={styles.formItemCol}>
+                  <IceIcon type="lock" size="small" style={styles.inputIcon} />
+                  <IceFormBinder name="password" required message="请输入密码">
+                    <Input
+                      size="large"
+                      htmlType="password"
+                      placeholder="密码"
+                    />
+                  </IceFormBinder>
+                </Col>
+                <Col>
+                  <IceFormError name="password" />
+                </Col>
+              </Row>
+
+              <Row style={styles.formItem}>
+                <Button
+                  type="primary"
+                  onClick={this.handleSubmit}
+                  style={styles.submitBtn}
+                >
+                  登 录
+                </Button>
+              </Row>
+            </div>
+          </IceFormBinderWrapper>
+        </div>
+      </div>
+    );
+  }
+}
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import { Redirect } from 'react-router-dom'
 
-function getAuth() {
-  return true
+//获取权限列表，权限列表存储在sessionStorage中
+export function getAuth() {
+  const authList = sessionStorage.getItem('authList')
+  return authList || false
 }
+
+//移除权限列表
+export function removeAuth(){
+  sessionStorage.removeItem('authList')
+}
+
+/*
+* 权限包装的HOC
+* @param {Function} auth 权限函数，返回一个Boolean，表明是否有权限
+* @param {ReactNode} Component, react组件，被包装的权限组件
+* */
 export function withAuth(auth) {
   return function(Component) {
     return class extends React.Component {
