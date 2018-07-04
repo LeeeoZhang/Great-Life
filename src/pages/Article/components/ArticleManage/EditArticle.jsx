3eb0ca0dda9db19535420c5a3f3a3e34467856fd
leo <leozhang621@gmail.com>
2018-07-04 19:29:05 +0800

    add editArticle

--- src/pages/Article/components/ArticleManage/EditArticle.jsx
+++ src/pages/Article/components/ArticleManage/EditArticle.jsx
@@ -1,1 +1,9 @@
-import React , {Fragment} from 'react'
+import React, {Fragment} from 'react'
+import {
+  FormBinderWrapper as IceFormBinderWrapper,
+  FormBinder as IceFormBinder,
+  FormError as IceFormError,
+} from '@icedesign/form-binder'
+import {Grid, Input, Button, Feedback} from "@icedesign/base"
+import BraftEditor from 'braft-editor'
+import 'braft-editor/dist/braft.css'
